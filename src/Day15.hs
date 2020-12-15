23732eae054aea153c1a9b4e23ea3b66ca0fb64a
Victor Nascimento Bakke <gipphe@gmail.com>
2020-12-15 16:17:40 +0100

    perf: use mutable, unboxed vectors for Day15

--- src/Day15.hs
+++ src/Day15.hs
@@ -1,15 +1,27 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE ViewPatterns #-}
 {-# LANGUAGE TemplateHaskell #-}
 
 module Day15 where
 
-import Data.FileEmbed (embedStringFile)
 import Control.DeepSeq (force)
+import Control.Monad.ST (ST, runST)
+import Data.FileEmbed (embedStringFile)
+import Data.Foldable (foldlM, toList)
 import Data.IntMap (IntMap)
 import qualified Data.IntMap.Strict as M
+import Data.Sequence (Seq(..))
+import qualified Data.Sequence as Seq
+import Data.Vector.Unboxed.Mutable (MVector)
+import qualified Data.Vector.Unboxed.Mutable as V
 
 import Util (SomeDay(..), Day, Task, runTask, split)
+
+
+----------------
+-- * Boilerplate
+----------------
 
 someDay15 :: SomeDay
 someDay15 = SomeDay day15
@@ -19,50 +31,66 @@ day15 = do
     runTask day15Task1
     runTask day15Task2
 
-parseInput :: String -> [Int]
-parseInput = fmap read . split ','
-
-rawInput :: String
-rawInput = $(embedStringFile "input/day15.txt")
-
-parsedInput :: [Int]
-parsedInput = parseInput rawInput
-
 day15Task1 :: Task 1 Int
 day15Task1 = pure $ computeTask1 parsedInput
-
-computeTask1 :: [Int] -> Int
-computeTask1 = compute 2020
 
 day15Task2 :: Task 2 Int
 day15Task2 = pure $ computeTask2 parsedInput
 
-computeTask2 :: [Int] -> Int
-computeTask2 = compute 30000000
 
+---------
+-- * Work
+---------
+
+computeTask1 :: Seq Int -> Int
+computeTask1 = computeV 2020
+
+computeTask2 :: Seq Int -> Int
+computeTask2 = computeV 30000000
+
+-- | Unboxed, mutable vector solution
+computeV :: Int -> Seq Int -> Int
+computeV limit (toList -> initial) = runST $ do
+    vector <- V.new limit
+    mapM_ (uncurry $ V.write vector) $ zip (init initial) [1 ..]
+    foldlM (stepV vector) (last initial) [length initial .. limit - 1]
+
+stepV :: MVector s Int -> Int -> Int -> ST s Int
+stepV vector num counter = do
+    lastCount <- V.read vector num
+    V.write vector num counter
+    if lastCount == 0 then pure 0 else pure $ counter - lastCount
+
+-- | Previous solution
 compute :: Int -> [Int] -> Int
 compute _     []  = error "No numbers"
-compute limit xs' = go limit startTurn x' $ M.fromList $ fmap (fmap pure) $ zip
+compute limit xs' = step limit startTurn x' $ M.fromList $ fmap (fmap pure) $ zip
     xs'
-    [1 ..]
+    [1 .. length xs']
   where
     startTurn = length xs' + 1
     x'        = last xs'
 
-go :: Int
-   -- ^ Turn limit
-   -> Int
-   -- ^ Current turn number
-   -> Int
-   -- ^ Current number that we are evaluating
-   -> IntMap [Int]
-   -- ^ Map of previously spoken numbers and the turns they were spoken
-   -> Int
-go limit !currentTurn !x !m
-    | currentTurn <= limit = go limit (currentTurn + 1) newNumber m'
+step :: Int -> Int -> Int -> IntMap [Int] -> Int
+step limit !currentTurn !x !m
+    | currentTurn <= limit = step limit (currentTurn + 1) newNumber m'
     | otherwise            = x
   where
     newNumber = maybe 0 handleNewNumber $ M.lookup x m
     m'        = force $ M.insertWith (<>) newNumber [currentTurn] m
     handleNewNumber (lastTurn : turnBefore : _) = lastTurn - turnBefore
     handleNewNumber _                           = 0
+
+
+----------
+-- * Input
+----------
+
+parseInput :: String -> Seq Int
+parseInput = Seq.fromList . fmap read . split ','
+
+rawInput :: String
+rawInput = $(embedStringFile "input/day15.txt")
+
+parsedInput :: Seq Int
+parsedInput = parseInput rawInput
--- src/Util.hs
+++ src/Util.hs
@@ -5,10 +5,13 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE ExistentialQuantification #-}
+{-# OPTIONS_GHC -fno-warn-unused-top-binds #-}
 
 module Util
     ( Day
+    , runDay'
     , Task
+    , runTask'
     , SomeDay(..)
     , runSomeDay
     , runDay
@@ -32,28 +35,30 @@ import GHC.TypeLits (KnownNat, Nat, natVal)
 -- * Nifty monads to automate console logging
 ---------------------------------------------
 
+-- | Exsistential type to hold each day, since their day numbers are a part of
+-- their type.
 data SomeDay = forall n . KnownNat n => SomeDay (Day n ())
 
 runSomeDay :: SomeDay -> IO ()
 runSomeDay (SomeDay day) = runDay day
 
+-- | A given day with its day number.
 newtype Day (n :: Nat) a = Day { runDay' :: IO a }
-    deriving (Functor, Applicative, Monad) via IO
+    deriving (Functor, Applicative, Monad, MonadIO) via IO
 
-instance MonadIO (Day n) where
-    liftIO = Day
-
+-- | Run a day, logging the day number.
 runDay :: forall n a . KnownNat n => Day n a -> IO a
 runDay m = do
     putStrLn $ "Day " <> show (natVal (Proxy @n))
     runDay' m
 
+-- | A day's task with the task number. Usually 1 or 2, since Advent of Code
+-- only has 2 tasks per day.
 newtype Task (n :: Nat) a = Task { runTask' :: IO a }
-    deriving (Functor, Applicative, Monad, MonadFail) via IO
+    deriving (Functor, Applicative, Monad, MonadFail, MonadIO) via IO
 
-instance MonadIO (Task n) where
-    liftIO = Task
-
+-- | Run a task and time its execution, rather roughly, logging the execution
+-- time.
 runTask :: forall n m a
          . (KnownNat n, Show a, NFData a)
         => Task n a
